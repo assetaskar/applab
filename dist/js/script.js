58273585ec3110e1094dfabfbbffb9f3667e0f48
Askar Assetov <comgraph.aa@gmail.com>
2020-12-25 19:28:43 +0600

    js

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,3 +4,50 @@ var mySwiper = new Swiper(".swiper-container", {
 		prevEl: ".swiper-button-prev",
 	},
 });
+
+const divPricePlan = document.querySelector(".price__plan");
+
+const priceBusiness = document.getElementById("business-class");
+const priceMaster = document.getElementById("pro-master");
+
+const plan = {
+	monthly: {
+		business: 29,
+		master: 99,
+	},
+	annually: {
+		business: 19,
+		master: 89,
+	},
+};
+
+function selectionPlan(price = plan.monthly, className = false) {
+	priceBusiness.innerHTML = "$" + price.business;
+	priceMaster.innerHTML = "$" + price.master;
+
+	className
+		? divPricePlan.classList.add(className)
+		: (divPricePlan.classList = divPricePlan.classList.item(0));
+}
+selectionPlan();
+
+divPricePlan.addEventListener("click", event => {
+	if (event.target.tagName === "DIV") return;
+
+	event.target.id === "annually"
+		? selectionPlan(plan[event.target.id], event.target.id)
+		: selectionPlan();
+});
+
+const faq = document.querySelector(".faq__list");
+const faqItems = document.querySelectorAll(".faq__item");
+
+faq.addEventListener("click", event => {
+	if (event.target.tagName !== "H3") return;
+
+	faqItems.forEach(question => {
+		question === event.target.closest(".faq__item")
+			? question.classList.add("show")
+			: question.classList.remove("show");
+	});
+});
